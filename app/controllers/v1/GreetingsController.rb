206ccd172e26b1fc4d2ecb161db085fcf51ebffb
Maclenn77 <maclenn77@gmail.com>
2022-04-05 14:24:30 -0500

    Add random greetings in API Endpoint

--- app/controllers/v1/GreetingsController.rb
+++ app/controllers/v1/GreetingsController.rb
@@ -1,7 +1,14 @@
 class V1::GreetingsController < ApplicationController
   def index
     render json: {
-        :greetings => 'A greeting'
+        :greetings => select_a_greeting
     }.to_json
+  end
+
+  private
+
+  def select_a_greeting
+    greetings = ['¡Hola! Bienvenidos.', '안녕하세요', 'Hello, welcome!', 'Здравствуйте!', 'こんにちは']
+    greetings[rand(5)]
   end
 end

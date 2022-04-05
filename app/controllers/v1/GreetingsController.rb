9ab51fb58b2375f126c12ee1c3bcd4d84f9db7c3
Maclenn77 <maclenn77@gmail.com>
2022-04-05 14:54:24 -0500

    Create react component App

--- app/controllers/v1/GreetingsController.rb
+++ app/controllers/v1/GreetingsController.rb
@@ -1,13 +1,13 @@
 class V1::GreetingsController < ApplicationController
   def index
     render json: {
-        :greetings => select_a_greeting
+        :greetings => a_random_greeting
     }.to_json
   end
 
   private
 
-  def select_a_greeting
+  def a_random_greeting
     greetings = ['¡Hola! Bienvenidos.', '안녕하세요', 'Hello, welcome!', 'Здравствуйте!', 'こんにちは']
     greetings[rand(5)]
   end
